2fc3266bf0df2c15555f08203718ae762b12259e
Aashay <arkhamknight.aashay@gmail.com>
2022-03-19 13:04:32 +0530

    added alt for images

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -13,7 +13,7 @@ const LandingPage = () => {
     return (
         <div className='page'>
             <div className='section2'>
-                <img src={dots} id="dots" />
+                <img src={dots} id="dots" alt="dots" />
                 <div className='counting'>
                     <div id="circle"></div>
 
@@ -43,14 +43,14 @@ const LandingPage = () => {
             </div>
 
             <div className='verification'>
-                <img src={illustration} id="illustration" />
+                <img src={illustration} id="illustration" alt="illustration"/>
                 <div className='verification-content'>
                     <span>DEFI</span>
                     <h2>Scale the world of DeFi</h2>
                     <p>Being the veterans in the blockchain industry, we understand the hype of decentralized finance and the contemporary optimizations in the financial sector and hence taking the grasp of the moment , we have developed exceptional projects in the world of DeFi.
                     </p>
                 </div>
-                <img src={neon1} id="neon1" />
+                <img src={neon1} id="neon1" alt="neon1" />
             </div>
 
             <div className='avoid-costly-errors'>
@@ -59,18 +59,18 @@ const LandingPage = () => {
                     <h2>Technology made easier</h2>
                     <p>Decentralized applications have found a huge user base right in the initial stage , and therefore we as a team have gained extensive expertise into the creation of dapps.</p>
                 </div>
-                <img src={maintenance} id="maintenance" />
+                <img src={maintenance} id="maintenance" alt="maintenance"/>
             </div>
 
             <div className='detailed'>
-                <img src={development} id="development" />
+                <img src={development} id="development" alt="development" />
                 <div className='detailed-content'>
                     <span>AUDIT</span>
                     <h2>Stay secure and safe</h2>
                     <p>Above all that matters is a secure and safe smart contract that is free from any vulnerability and does not hamper the investment of the various people who have trusted you. We also provide auditing services , so that you can stay assured of the security as we are quick in auditing and exposing the bugs of your smart contract.
                     </p>
                 </div>
-                <img src={neon1} id="neon2" />
+                <img src={neon1} id="neon2" alt="neon2"/>
 
             </div>
 
@@ -80,19 +80,19 @@ const LandingPage = () => {
                     <h2>Expert Review</h2>
                     <p>Experience the guidance of various trained professionals and developers who are with you at every phase of your projects. We not only develop your project but also give out the best recommendations for turning it into a huge success</p>
                 </div>
-                <img src={discuss} id="discuss" />
+                <img src={discuss} id="discuss" alt="discuss"/>
 
             </div>
 
 
             <div className='easy-integration'>
-                <img src={work} id="work" />
+                <img src={work} id="work" alt="work"/>
                 <div className='easy-integration-content'>
                     <span>SMART CONTRACT</span>
                     <h2>Experience the Revolution</h2>
                     <p>Merklabs has efficient developers with exceptional coding skills that can create any type of smart contract you desire and with scalable and secure code. You desire, we create it for you.</p>
                 </div>
-                <img src={neon1} id="neon1" />
+                <img src={neon1} id="neon1" alt="neon1" />
 
             </div>
 
@@ -103,7 +103,7 @@ const LandingPage = () => {
                     <h2>Stabilize and Promote</h2>
                     <p>Amidst the competition in the Defi space, good marketing strategy and implementation plays the key role to stabilize and promote your growth in the blockchain industry. Retaining this awareness, we can develop launchpad and IDO for your presale and ICO. </p>
                 </div>
-                <img src={scene} id="scene" />
+                <img src={scene} id="scene" alt="scene" />
             </div>
         </div>
     )
